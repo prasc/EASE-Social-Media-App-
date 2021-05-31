1910a6133f765afccf8a6c7646f9f1f43aa43d62
prashathc <prashathc@gmail.com>
2021-05-30 22:59:56 -0400

    set up back end logic and starting front end

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -3,17 +3,26 @@ import PostMessage from '../models/postMessage';
 // These are handlers for all of our routes
 // It handles the logic for all our routes
 
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
     // Find is an async function
     try {
-        const postMessage = await PostMessage.find();
+         const postMessage = await PostMessage.find();
 
         res.status(200).console.log(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message});
     }
-})
+};
 
 export const createPost = (req, res) => {
-    res.send("Post Creation!");
-})
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    
+    try {
+            await newPost.save();
+            res.status(201).json(newPost);
+   } catch (error) {
+        res.status(404).json({ message: error.message});
+   }
+};
