f75641c874550c2c7e814ebefff8eee0bfb32433
prashathc <prashathc@gmail.com>
2021-06-18 18:55:00 -0400

    alot of bugs to fix

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
+
 
 // connect to MongoDB cluster
 const CONNECTION_URL = 'mongodb+srv://pras:pras123@cluster0.mwzgy.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
