c8d5a8c93de3007f9e75c325c23c4c778cbb6b4f
prashathc <prashathc@gmail.com>
2021-06-21 10:03:56 -0400

    basic function working

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
-    creator: String,
-    tags: [String],
-    selectedFile: String,
+    user: String,
     likeCount: {
         type: Number,
         default: 0,
